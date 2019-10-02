11606f3af44616c84af130b1900bcdc1b0e72cef
Jiwon Jeon <jwurbane97@ajou.ac.kr>
2019-10-02 19:48:37 +0900

    add: Visualization modules

--- pages/service/intelligences/analytics.vue
+++ pages/service/intelligences/analytics.vue
@@ -3,12 +3,12 @@
         <div class="container-wrapper" v-if='chkError || getCount === 0'>
             <header data-role="app-title">커뮤니티 게시물<br><strong>수집데이터 분석</strong></header>
             <Message icon="fas fa-exclamation-triangle" alert="uk-alert-danger" message="데이터베이스 연결에 실패하였습니다." v-show="chkError" :class="{ 'valign-center': true, 'full-width': true }" />
-            <Message icon="fas fa-exclamation-triangle" alert="uk-alert-danger" message="데이터가 없습니다." v-show="!chkError" />
+            <Message icon="fas fa-exclamation-triangle" alert="uk-alert-danger" message="데이터가 없습니다." v-show="!chkError" :class="{ 'valign-center': true, 'full-width': true }" />
         </div>
         <div class="container-wrapper" v-else>
-          <!-- <Message icon="fas fa-exclamation-triangle" alert="uk-alert-danger" message="시각화 모듈이 현재 실행 상태가 아닙니다." :class="{ 'valign-center': true, 'full-width': true }" /> -->
+          <header data-role="app-title">커뮤니티 게시물<br><strong>수집데이터 분석</strong></header>
           <ECharts
-            :options="pie"
+            :options="options"
             :init-options="initOptions"
             autoresize
           />
@@ -20,7 +20,27 @@
 import axios from 'axios'
 import Message from '~/components/Message.vue'
 import ECharts from '~/components/ECharts.vue'
+
+import 'echarts-liquidfill'
+import 'echarts-wordcloud'
+
+import '~/node_modules/echarts/lib/chart/bar'
+import '~/node_modules/echarts/lib/chart/line'
 import '~/node_modules/echarts/lib/chart/pie'
+import '~/node_modules/echarts/lib/chart/map'
+import '~/node_modules/echarts/lib/chart/radar'
+import '~/node_modules/echarts/lib/chart/scatter'
+import '~/node_modules/echarts/lib/chart/effectScatter'
+import '~/node_modules/echarts/lib/component/tooltip'
+import '~/node_modules/echarts/lib/component/polar'
+import '~/node_modules/echarts/lib/component/geo'
+import '~/node_modules/echarts/lib/component/legend'
+import '~/node_modules/echarts/lib/component/title'
+import '~/node_modules/echarts/lib/component/visualMap'
+import '~/node_modules/echarts/lib/component/dataset'
+import '~/node_modules/echarts/map/js/world'
+
+import 'zrender/lib/svg/svg'
 
 export default {
   layout: 'service',
@@ -36,19 +56,25 @@ export default {
       return this.$store.getters['intelligences/chkError']
     }
   },
-  beforeMount () {
-  },
   head () {
     return {
       title: '커뮤니티게시물 특이동향 관리 :: 커뮤니티게시물모니터링포털'
     }
   },
+  beforeMount () {
+    this.$store.dispatch('intelligences/getData')
+  },
   data () {
     return {
+      options: {},
       initOptions: {
-        renderer: 'canvas'
+        renderer: 'svg'
       }
     }
+  },
+  async asyncData () {
+    let options = await axios.get('/api/v2/visualize/pie')
+    return { options: options.data }
   }
 }
 </script>
--- server/routes/v2/visualizations.js
+++ server/routes/v2/visualizations.js
@@ -2,29 +2,196 @@ const { Router } = require('express')
 
 const router = Router()
 
+const Intelligences = require('../../models/intelligence')
+
 const jsonStrings = {
     title: 'Communities Monitoring Works Portal V2',
     msg_ok: 'ok',
     msg_notFound: 'Intelligence Not Found'
 }
 
-router.get('/v2/visualize/wordcloud', (req, res) => {
+router.get('/v2/visualize/wordcloud', async (req, res) => {
+  res.status(200).json({
+    tooltip: {},
+    series: [ {
+        type: 'wordCloud',
+        gridSize: 2,
+        sizeRange: [12, 50],
+        rotationRange: [-90, 90],
+        shape: 'pentagon',
+        width: 600,
+        height: 400,
+        drawOutOfBound: true,
+        textStyle: {
+            normal: {
+                color: function () {
+                    return 'rgb(' + [
+                        Math.round(Math.random() * 160),
+                        Math.round(Math.random() * 160),
+                        Math.round(Math.random() * 160)
+                    ].join(',') + ')';
+                }
+            },
+            emphasis: {
+                shadowBlur: 10,
+                shadowColor: '#333'
+            }
+        },
+        data: [
+            {
+                name: 'Sam S Club',
+                value: 10000,
+                textStyle: {
+                    normal: {
+                        color: 'black'
+                    },
+                    emphasis: {
+                        color: 'red'
+                    }
+                }
+            },
+            {
+                name: 'Macys',
+                value: 6181
+            },
+            {
+                name: 'Amy Schumer',
+                value: 4386
+            },
+            {
+                name: 'Jurassic World',
+                value: 4055
+            },
+            {
+                name: 'Charter Communications',
+                value: 2467
+            },
+            {
+                name: 'Chick Fil A',
+                value: 2244
+            },
+            {
+                name: 'Planet Fitness',
+                value: 1898
+            },
+            {
+                name: 'Pitch Perfect',
+                value: 1484
+            },
+            {
+                name: 'Express',
+                value: 1112
+            },
+            {
+                name: 'Home',
+                value: 965
+            },
+            {
+                name: 'Johnny Depp',
+                value: 847
+            },
+            {
+                name: 'Lena Dunham',
+                value: 582
+            },
+            {
+                name: 'Lewis Hamilton',
+                value: 555
+            },
+            {
+                name: 'KXAN',
+                value: 550
+            },
+            {
+                name: 'Mary Ellen Mark',
+                value: 462
+            },
+            {
+                name: 'Farrah Abraham',
+                value: 366
+            },
+            {
+                name: 'Rita Ora',
+                value: 360
+            },
+            {
+                name: 'Serena Williams',
+                value: 282
+            },
+            {
+                name: 'NCAA baseball tournament',
+                value: 273
+            },
+            {
+                name: 'Point Break',
+                value: 265
+            }
+        ]
+    } ]
+  })
+})
+
+router.get('/v2/visualize/metric', async (req, res) => {
     
 })
 
-router.get('/v2/visualize/metric', (req, res) => {
-    
+router.get('/v2/visualize/bar', async (req, res) => {
+    let agg = await Intelligences.aggregate([
+      {
+        $group: {
+          _id: '$item.meta.site',
+          count: { $sum: 1 }
+        }
+      }
+    ]).exec()
+
+    res.status(200).json({ data: agg })
 })
 
-router.get('/v2/visualize/bar', (req, res) => {
-    
+router.get('/v2/visualize/pie', async (req, res) => {
+  let agg = await Intelligences.aggregate([
+    {
+      $group: {
+        _id: '$item.meta.site',
+        count: { $sum: 1 }
+      }
+    }
+  ]).exec()
+
+  res.status(200).json({
+    title: {
+      text: '주요 특이동향 게시물 스크랩 출처',
+      x: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <hr/>{b} : {c} ({d}%)'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: agg.map(item => item._id)
+    },
+    series: [
+      {
+        name: '사이트명',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '60%'],
+        data: agg.map((item) => ({ value: item.count, name: item._id })),
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 5,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, .12)'
+          }
+        }
+      }
+    ]
+  })
 })
 
-router.get('/v2/visualize/pie', (req, res) => {
-    
-})
-
-router.get('/v2/visualize/map', (req, res) => {
+router.get('/v2/visualize/map', async (req, res) => {
     
 })
 
